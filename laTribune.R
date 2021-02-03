b5ead839e2e7d68da166f7ca1f77a8390bf44707
Mouss1995 <cuzonalan@gmail.com>
2021-02-03 16:22:25 +0100

    Première fonction pour le journal La Tribune qui récupère les urls des articles présents en première page de la catégorie "Économie".

---
+++ laTribune.R
@@ -0,0 +1,5 @@
+# laTribuneScrap <- function(url = "https://www.latribune.fr/actualites/economie/economie.html"){
+#   tribune_articles <- read_html(url)
+#   tribune_urls <- tribune_articles %>% html_nodes("article") %>% html_nodes("a") %>% html_attr("href")
+#   unique(tribune_urls)
+#   }
